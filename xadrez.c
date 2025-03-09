52a7e7c6213746902dedc4566b9dc1604507e96e
Rafael Carvalho <202503257061@alunos.estacio.br>
2025-03-09 13:02:26 -0300

    Update xadrez.c adicao de cavalo com loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,8 @@ int main() {
     int casasTorre = 5;
     int casasBispo = 5;
     int casasRainha = 8;
+    int casasCavaloVertical = 2; // Duas casas para baixo
+    int casasCavaloHorizontal = 1; // Uma casa para a esquerda
     
     // Movimento da Torre (linha reta) usando for
     printf("\nMovimento da Torre:\n");
@@ -27,6 +29,18 @@ int main() {
         printf("Esquerda\n"); // Movimenta a rainha para a esquerda
         k++; // Incrementa a variavel de controle
     } while (k < casasRainha); // Repete enquanto k for menor que casasRainha
+    
+    // Movimento do Cavalo (em "L") usando loops aninhados
+    printf("\nMovimento do Cavalo:\n");
+    for (int m = 0; m < casasCavaloVertical; m++) { // Movimenta duas casas para baixo
+        printf("Baixo\n");
+    }
+    
+    int n = 0; // Inicializa variável para controle do segundo loop
+    while (n < casasCavaloHorizontal) { // Movimenta uma casa para a esquerda
+        printf("Esquerda\n");
+        n++;
+    }
     
     return 0; // Finaliza o programa
 }
